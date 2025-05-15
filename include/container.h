84e40e689afe203a69648e3ce096660dc17bf0aa
jiahao-fang <416189752@qq.com>
2025-05-15 14:45:03 +0000

    ring buffer logic fix& O3 compile

--- include/container.h
+++ include/container.h
@@ -2,40 +2,61 @@
 
 #include "type_def.h"
 #include <vector>
-
+#include <iostream>
+#include <cassert>
 namespace pnl {
 
 // Ring buffer implementation using vector
 class RingBuffer {
 public:
+    explicit RingBuffer(size_t initial_capacity = 256)
+        : data_(initial_capacity), capacity_(initial_capacity) {
+        // Ensure capacity is a power of 2
+        assert((capacity_ & (capacity_ - 1)) == 0 && "Capacity must be a power of 2");
+    }
+
     void push(const Position& pos) {
-        if (size_ == data_.size()) {
-            data_.push_back(pos);
-        } else {
-            data_[tail_] = pos;
+        if (size_ == capacity_) {
+            resize(capacity_ << 1); // Multiply by 2 using bit shift
         }
-        tail_ = (tail_ + 1) % data_.size();
-        size_++;
+        data_[tail_] = pos;
+        tail_ = (tail_ + 1) & (capacity_ - 1); // Fast modulo for power of 2
+        ++size_;
+        
     }
 
     Position& front() {
+        if (empty()) throw std::runtime_error("front from empty buffer");
         return data_[head_];
     }
 
     void pop() {
-        head_ = (head_ + 1) % data_.size();
-        size_--;
+        if (empty()) throw std::runtime_error("pop from empty buffer");
+        head_ = (head_ + 1) & (capacity_ - 1); // Fast modulo for power of 2
+        --size_;
     }
 
-    bool empty() const {
-        return size_ == 0;
-    }
+    bool empty() const { return size_ == 0; }
+    size_t size() const { return size_; }
+    size_t capacity() const { return capacity_; }
 
 private:
+    void resize(size_t new_capacity) {
+        std::vector<Position> new_data(new_capacity);
+        for (size_t i = 0; i < size_; ++i) {
+            new_data[i] = data_[(head_ + i) % capacity_];
+        }
+        data_ = std::move(new_data);
+        head_ = 0;
+        tail_ = size_;
+        capacity_ = new_capacity;
+    }
+
     std::vector<Position> data_;
     size_t head_ = 0;
     size_t tail_ = 0;
     size_t size_ = 0;
+    size_t capacity_;
 };
 
 // Stack implementation using vector
