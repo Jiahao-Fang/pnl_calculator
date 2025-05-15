2c1e5c105bb64e40a3c65a2b618e18526c62c470
jiahao-fang <416189752@qq.com>
2025-05-15 14:03:49 +0000

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/ebs1/ebs/pnl_calculator
-# Build directory: /mnt/ebs1/ebs/pnl_calculator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(pnl_calculator_test "/mnt/ebs1/ebs/pnl_calculator/build/test_pnl_calculator")
-set_tests_properties(pnl_calculator_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/ebs1/ebs/pnl_calculator/CMakeLists.txt;40;add_test;/mnt/ebs1/ebs/pnl_calculator/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
